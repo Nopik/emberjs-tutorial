675f714e364f646f656f637bb2a386511b2bf6c3
Kamil Burzynski <kamil@nopik.net>
2012-09-09 21:17:17 +0200

    Initial version.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberjsTutorial::Application.config.secret_token = 'd0bde6394cdf9f2881ce9c9177cb746c1fa9cab7c853d1ee4603d689c339e4f70bf9ab196296546df08013408f69d7fa1c59e7346dbe8291636317589036637d'
